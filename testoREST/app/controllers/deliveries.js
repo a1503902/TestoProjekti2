a60eea58ce82116245e4782c2428844cadce7759
ristoveijonen <risto.veijonen@outlook.com>
2017-01-26 15:23:22 +0200

    Delivery model

--- testoREST/app/controllers/deliveries.js
+++ testoREST/app/controllers/deliveries.js
@@ -1,24 +1,82 @@
-var express = require('express');
-var router  = express.Router();
+var express 	= require('express');
+var router  	= express.Router();
+var Delivery 	= require('../models/delivery');
 
 // Get all deliveries
-router.get('/', function(req, res){
+router.get('/deliveries', function(req, res){
 	res.send('Get all deliveries');
 });
 
+// Find by ID ALL
+router.get('/deliveries/:deliveryID',function(req, res) {
+	Delivery.findById(req.params.deliveryID, function(err, delivery) {
+		if (err)
+			res.send(err);
+		res.json(delivery);
+	});
+});
+
+// Find by ID name
+router.get('/deliveries/:deliveryID/name',function(req, res) {
+    Delivery.findById(req.params.deliveryID, 'name', function(err, delivery) {
+        if (err)
+            res.send({success: false, error: err});
+        res.json({
+			success: true,
+			data: delivery
+        });
+    });
+});
+
+// Find by ID delivery
+router.get('/deliveries/:deliveryID/delivery',function(req, res) {
+    Delivery.findById(req.params.deliveryID, 'delivery', function(err, delivery) {
+        if (err)
+            res.send({success: false, error: err});
+        res.json({
+            success: true,
+            data: delivery
+        });
+    });
+});
+
+// Find by ID pickup
+router.get('/deliveries/:deliveryID/pickup',function(req, res) {
+    Delivery.findById(req.params.deliveryID, 'pickup', function(err, delivery) {
+        if (err)
+            res.send({success: false, error: err});
+        res.json({
+            success: true,
+            data: delivery
+        });
+    });
+});
+
+// Find by ID unknown
+router.get('/deliveries/:deliveryID/unknown',function(req, res) {
+    Delivery.findById(req.params.deliveryID, 'unknown', function(err, delivery) {
+        if (err)
+            res.send({success: false, error: err});
+        res.json({
+            success: true,
+            data: delivery
+        });
+    });
+});
+
 // Insert new delivery
-router.post('/', function(req, res){
+router.post('/deliveries', function(req, res){
 	res.send('Insert delivery');
 });
 
 // Update delivery details
-router.put('/:deliveryId', function(req, res){
+router.put('/deliveries/:deliveryId', function(req, res){
 	res.send(req.params);
 	res.send('Update delivery');
 });
 
 // Delete delivery
-router.delete('/:deliveryId', function(req, res){
+router.delete('/deliveries/:deliveryId', function(req, res){
 	res.send(req.params);
 	res.send('Delete delivery');
 });
---
+++ testoREST/app/models/delivery.js
@@ -0,0 +1,11 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var DeliverySchema   = new Schema({
+    name: String,
+    delivery: Number,
+    pickup: Number,
+    unknown: Number
+});
+
+module.exports = mongoose.model('Car', DeliverySchema   );
