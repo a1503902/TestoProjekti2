c808c2d8d76c5297afaa0b9765899e0dfc277cac
Niko Tikkanen <niko@plexcellmedia.com>
2017-01-30 16:06:08 +0200

    login fix

--- Fronttifilut/assets/js/login.js
+++ Fronttifilut/assets/js/login.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
+
+
+	/*
     $("#login").on( "submit", function(event) {
         event.preventDefault()
         $.post('localhost:8080/api/authenticate', $('#login').serialize())
 })
+	*/
 })
