62571ad92a60089ef415a5977c6dd91e68ffa62e
ristoveijonen <risto.veijonen@outlook.com>
2017-01-30 14:16:45 +0200

    REST deliveries

--- testoREST/app/models/delivery.js
+++ testoREST/app/models/delivery.js
@@ -7,9 +7,4 @@ var DeliverySchema   = new Schema({
     pickup: Number,
     unknown: Number
 });
-
-<<<<<<< HEAD
 module.exports = mongoose.model('Delivery', DeliverySchema);
-=======
-module.exports = mongoose.model('Delivery', DeliverySchema);
->>>>>>> dfcc3a8761ef704f925bdf4ac0bd6bb8eaede28f
