a960abed386dbbc6b9ed0a553e5c11db8f90d3d8
Mikael <mikael.vilen@gmail.com>
2017-01-30 16:12:20 +0200

    update

--- Fronttifilut/assets/js/login.js
+++ Fronttifilut/assets/js/login.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
 	$("#login").click(function(){
-	      var username = $("input[name=username]").val();
-	      var password = $("input[name=password]").val();
+	      var username = $("input[name=username]").val()
+	      var password = $("input[name=password]").val()
 	      $.ajax({
 	        method: 'POST',
 	        type: 'JSON',
 	        url: 'http://localhost:8080/api/authenticate',
 	        data: {username: username, password: password},
 	        success: function(data){
-	          alert(JSON.stringify(data));
+	          alert(JSON.stringify(data))
 	        },
 	        error: function(err){
-	          alert(JSON.stringify(err));
+	          alert(JSON.stringify(err))
 	        }
 	      })
-	    });
+	    })
 })
