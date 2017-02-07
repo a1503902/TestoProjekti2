2bdeab81bfefb83b008e012c3feb1dcfb9264000
anttiwest <antti_west@msn.com>
2017-02-07 14:28:09 +0200

    merged to master

--- testoREST/app/controllers/front.js
+++ testoREST/app/controllers/front.js
@@ -3,15 +3,15 @@ var router  = express.Router();
 var pug     = require('pug');
 
 router.get('/', function(req, res){
-	res.render('admin/login', { title: 'login', message: 'Hello there!'});
+	res.render('login', { title: 'login', message: 'Hello there!'});
 });
 
 router.get('/start', function(req, res){
-    res.render('views/startpage', { title: 'Start'});
+    res.render('startpage', { title: 'Start'});
 });
 
 router.get('/final', function(req, res){
-    res.render('views/finalpage', { title: 'final'});
+    res.render('finalpage', { title: 'final'});
 });
 
 module.exports = router;
